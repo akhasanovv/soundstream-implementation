06e2eb179b8e8d4a6d4d8e73c3b292364ddce9c2
Aidar Khasanov <arkhasanov_1@edu.hse.ru>
2026-05-15 13:28:54 +0300

    added generator implemenrtation

---
+++ src/model/rvq.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, decay=0.99, epsilon=1e-5):
+        super().__init__()
+        
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.decay = decay
+        self.epsilon = epsilon
+
+        embed = torch.randn(num_embeddings, embedding_dim)
+        self.register_buffer("embed", embed)
+        
+        self.register_buffer("cluster_size", torch.zeros(num_embeddings))
+        self.register_buffer("embed_avg", embed.clone())
+
+    def forward(self, x): # (batch_size, emb_dim, time)
+        x_flat = x.permute(0, 2, 1).reshape(-1, self.embedding_dim) # (batch_size*time, emb_dim)
+
+        distances = (
+            torch.sum(x_flat**2, dim=1, keepdim=True)
+            + torch.sum(self.embed**2, dim=1)
+            - 2 * torch.matmul(x_flat, self.embed.t())
+        ) # ||a-b||_2^2 -> a in x, b in embeddings
+        indices = torch.argmin(distances, dim=1) 
+
+        z_q = F.embedding(indices, self.embed)
+        z_q = z_q.view(x.permute(0, 2, 1).shape)
+        z_q = z_q.permute(0, 2, 1).contiguous()
+
+        if self.training:
+            one_hot = F.one_hot(indices, num_classes=self.num_embeddings).type_as(x_flat)
+
+            batch_cluster_counts = one_hot.sum(dim=0)
+            batch_embedding_sums = one_hot.T @ x_flat
+
+            self.cluster_size.mul_(self.decay)
+            self.cluster_size.add_(batch_cluster_counts, alpha=1-self.decay)
+
+            self.embed_avg.mul_(self.decay)
+            self.embed_avg.add_(batch_embedding_sums, alpha=1-self.decay)
+
+            total_count = self.cluster_size.sum()
+            
+            smoothed_cluster_size = (self.cluster_size + self.epsilon) / (total_count + self.num_embeddings * self.epsilon) * total_count
+            self.embed.copy_(self.embed_avg / smoothed_cluster_size.unsqueeze(1))
+
+        loss = F.mse_loss(z_q.detach(), x)
+        z_q = x + (z_q - x).detach()
+
+        return z_q, loss, indices
+
+
+class ResidualVectorQuantizer(nn.Module):
+    """
+    RVQ model from [soundstream paper](https://arxiv.org/abs/2107.03312)
+    """
+    def __init__(self, num_quantizers, num_embeddings, embedding_dim):
+        super().__init__()
+
+        self.layers = nn.ModuleList([
+            VectorQuantizer(num_embeddings, embedding_dim) for _ in range(num_quantizers)
+        ])
+
+    def forward(self, x):
+        out = torch.zeros_like(x)
+        residual = x
+        all_losses = []
+        all_indices = []
+
+        for layer in self.layers:
+            z_q, loss, indices = layer(residual)
+            residual = residual - z_q
+            out = out + z_q
+            all_losses.append(loss)
+            all_indices.append(indices)
+
+        avg_loss = sum(all_losses) / len(all_losses)
+
+        return out, avg_loss, all_indices
---
+++ src/model/soundstream_model.py
@@ -0,0 +1,179 @@
+import torch.nn.functional as F
+from torch import nn
+
+from src.model.rvq import ResidualVectorQuantizer
+
+class CausalConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, stride=1):
+        super().__init__()
+        
+        self.left_padding = (kernel_size - 1) * dilation
+        self.conv = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            dilation=dilation,
+            stride=stride
+        )
+
+    def forward(self, x):
+        x = F.pad(x, (self.left_padding, 0))
+        return self.conv(x)
+
+
+class CausalConvTranspose(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super().__init__()
+        
+        self.trim_right = kernel_size - stride
+        self.conv = nn.ConvTranspose1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.trim_right > 0:
+            x = x[..., :-self.trim_right]
+        return x
+
+
+class ResidualUnit(nn.Module):
+    def __init__(self, channels, kernel_size=7, dilation=1):
+        super().__init__()
+        
+        self.layer = nn.Sequential(
+            CausalConv(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=kernel_size,
+                dilation=dilation
+            ),
+            nn.ELU(),
+            nn.Conv1d( # same as CausalConv, because pad=0
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=1
+            )
+        )
+
+    def forward(self, x):
+        return x + self.layer(x)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super().__init__()
+        
+        self.layers = nn.Sequential(
+            ResidualUnit(channels=in_channels, kernel_size=7, dilation=1),
+            ResidualUnit(channels=in_channels, kernel_size=7, dilation=3),
+            ResidualUnit(channels=in_channels, kernel_size=7, dilation=9),
+            nn.ELU(),
+            CausalConv(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=2*stride,
+                stride=stride
+            )
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.ELU(),
+            CausalConvTranspose(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=2 * stride,
+                stride=stride
+            ),
+            ResidualUnit(channels=out_channels, kernel_size=7, dilation=1),
+            ResidualUnit(channels=out_channels, kernel_size=7, dilation=3),
+            ResidualUnit(channels=out_channels, kernel_size=7, dilation=9),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            CausalConv(in_channels=1, out_channels=in_channels, kernel_size=7),
+            EncoderBlock(in_channels=in_channels, out_channels=2*in_channels, stride=2),
+            EncoderBlock(in_channels=2*in_channels, out_channels=4*in_channels, stride=4),
+            EncoderBlock(in_channels=4*in_channels, out_channels=8*in_channels, stride=5),
+            EncoderBlock(in_channels=8*in_channels, out_channels=16*in_channels, stride=5),
+            nn.ELU(),
+            CausalConv(
+                in_channels=16*in_channels, 
+                out_channels=out_channels, 
+                kernel_size=3
+            )
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        
+        self.layers = nn.Sequential(
+            CausalConv(in_channels=out_channels, out_channels=16*in_channels, kernel_size=7),
+            DecoderBlock(in_channels=16*in_channels, out_channels=8*in_channels, stride=5),
+            DecoderBlock(in_channels=8*in_channels, out_channels=4*in_channels, stride=5),
+            DecoderBlock(in_channels=4*in_channels, out_channels=2*in_channels, stride=4),
+            DecoderBlock(in_channels=2*in_channels, out_channels=in_channels, stride=2),
+            nn.ELU(),
+            CausalConv(
+                in_channels=in_channels, 
+                out_channels=1, 
+                kernel_size=7
+            )
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class SoundStreamModel(nn.Module):
+    """
+    SoundStream model from [paper](https://arxiv.org/abs/2107.03312)
+    """
+
+    def __init__(self, in_channels=32, out_channels=128, num_quantizers=8, num_embeddings=1024):
+        super().__init__()
+
+        self.encoder = Encoder(in_channels=in_channels, out_channels=out_channels)
+        self.rvq = ResidualVectorQuantizer(
+            num_quantizers=num_quantizers,
+            num_embeddings=num_embeddings,
+            embedding_dim=out_channels,
+        )
+
+        self.decoder = Decoder(in_channels=in_channels, out_channels=out_channels)
+
+    def forward(self, audio, **batch):
+        lat_x = self.encoder(audio)
+        quantized_lat, rvq_loss, rvq_indices = self.rvq(lat_x)
+        x_hat = self.decoder(quantized_lat)
+
+        return {
+            "reconstructed_audio": x_hat,
+            "latent": lat_x,
+            "quantized_latent": quantized_lat,
+            "commit_loss": rvq_loss,
+            "codebook_indices": rvq_indices
+        }
