46ece36d7f118adc7f4566a107ca665b9ae4baf3
Aidar Khasanov <arkhasanov_1@edu.hse.ru>
2026-05-15 13:49:03 +0300

    upd __init__ in models

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,11 @@
 from src.model.baseline_model import BaselineModel
+from src.model.discriminators import SoundStreamDiscriminator
+from src.model.rvq import ResidualVectorQuantizer
+from src.model.soundstream_model import SoundStreamModel
 
 __all__ = [
     "BaselineModel",
+    "SoundStreamModel",
+    "ResidualVectorQuantizer",
+    "SoundStreamDiscriminator",
 ]
